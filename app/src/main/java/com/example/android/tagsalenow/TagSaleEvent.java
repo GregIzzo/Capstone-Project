fd07798cebd5d718e0725181a8025e4170fbe841
Greg Izzo <greg.izzo@gmail.com>
2018-10-05 00:45:39 -0400

    feat: Added classes - FriendRecord, TagSaleEvent and UserData
    Built initial classes for data

---
+++ app/src/main/java/com/example/android/tagsalenow/TagSaleEvent.java
@@ -0,0 +1,68 @@
+package com.example.android.tagsalenow;
+
+/*
+event_id
+location_id
+user_id (tag sale owner)
+date
+start time
+end time
+description
+Tags
+event_change_counter - increments when owner makes a change
+
+ */
+public class TagSaleEvent {
+    private String locationId;
+    private String ownerId;
+    private String date;
+    private String startTime;
+    private String endTime;
+    private String description;
+    private String tags;
+    private int changeCounter =0;
+
+    public TagSaleEvent(){
+    }
+    public TagSaleEvent(String locationId, String ownerId, String date, String startTime, String endTime, String description, String tags){
+        this.locationId = locationId;
+        this.ownerId = ownerId;
+        this.date= date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.description = description;
+        this.tags = tags;
+        changeCounter = 0;
+    }
+    public String getLocationId() { return locationId;}
+
+    public void setLocationId(String locationId) { this.locationId = locationId;  }
+
+    public String getOwnerId() { return ownerId; }
+
+    public void setOwnerId(String ownerId) {  this.ownerId = ownerId; }
+
+    public String getDate() { return date;  }
+
+    public void setDate(String date) {  this.date = date;  }
+
+    public String getStartTime() {  return startTime; }
+
+    public void setStartTime(String startTime) {  this.startTime = startTime; }
+
+    public String getEndTime() { return endTime;  }
+
+    public void setEndTime(String endTime) {  this.endTime = endTime;  }
+
+    public String getDescription() {  return description; }
+
+    public void setDescription(String description) {   this.description = description;  }
+
+    public String getTags() {   return tags;  }
+
+    public void setTags(String tags) {   this.tags = tags;   }
+
+    public int getChangeCounter() {    return changeCounter;   }
+
+    public void setChangeCounter(int changeCounter) {   this.changeCounter = changeCounter;  }
+}
